2e692f5cb8dcb72018bcb0b7eaa6ca7ac2afab98
Michal <mpkowalcz@wp.pl>
2024-01-25 00:25:14 +0100

    Updated home page

--- MvcClinic/clinic-frontend-react/src/pages/Home.js
+++ MvcClinic/clinic-frontend-react/src/pages/Home.js
@@ -1,7 +1,11 @@
 function HomePage() {
     return (
-      <div>
-          Home Page
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-3 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h1 className="mt-10 text-center text-3xl font-bold leading-9 tracking-tight text-gray-900">
+            Welcome to clinic!
+          </h1>
+        </div>
       </div>
     );
  }
