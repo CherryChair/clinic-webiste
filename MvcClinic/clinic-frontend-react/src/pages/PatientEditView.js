a7dea8d112f22b0537a4d3f11d80c8ccd79479d9
Michal <mpkowalcz@wp.pl>
2024-01-25 21:15:46 +0100

    Added doctor view of parients

--- MvcClinic/clinic-frontend-react/src/pages/PatientEditView.js
+++ MvcClinic/clinic-frontend-react/src/pages/PatientEditView.js
@@ -2,18 +2,34 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 // import { isAdmin } from "./Login"
 import { useParams } from "react-router-dom";
+import ErrorBox from "../components/ErrorBox";
+import { isAdmin } from "./Login";
 
 
 export default function PatientEditPage() {
     const params = useParams();
     // let admin = isAdmin();
     const [patient, setPatient] = useState(Object());
+    const [errorFlag, setErrorFlag] = useState(false);
+    const [errorMsg, setErrorMsg] = useState("");
+
+    let doctor = !isAdmin();
 
     useEffect(() => {
         axios.get(`/Patients/?id=${params.id}`).then(response => {
             setPatient(response.data);
         }).catch(error => console.log(error));
     }, [params]);
+
+    const setError = (msg) => {
+        setErrorMsg(msg);
+        setErrorFlag(true);
+    };
+
+    const clearErrorFlag = () => {
+        setErrorMsg("");
+        setErrorFlag(false);
+    };
     
     const handleSubmit = event => {
         event.preventDefault();
@@ -32,8 +48,31 @@ export default function PatientEditPage() {
             setPatient(tempPat);
         }).catch(err => {
             console.log(err);
+            if (err.response.status === 409) {
+                setError("Patient data was changed");
+            } else {
+                setError("Not found");
+            }
         });
     };
+
+    const handleDelete = () => {
+        let patientPayload = {
+            id: patient.id,
+            concurrencyStamp: patient.concurrencyStamp,
+        }//pobieramy i ustawiamy nowe concurrency stamp
+
+        axios.post("/Patients/delete", patientPayload).then(response => {
+            window.location.href = "/patients";
+        }).catch(err => {
+            console.log(err);
+            if (err.response.status === 409) {
+                setError("Patient data was changed");
+            } else {
+                setError("Not found");
+            }
+        });
+    }
   
     return (
         <>
@@ -45,6 +84,7 @@ export default function PatientEditPage() {
           </div>
   
           <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
+            <ErrorBox errorFlag={errorFlag} changeErrorFlag={clearErrorFlag} errorMsg={errorMsg}/>
             {/* <form className="space-y-6" action="#" method="POST"> */}
             <form className="space-y-6" onSubmit={handleSubmit}>
               <div>
@@ -61,6 +101,7 @@ export default function PatientEditPage() {
                     defaultValue={patient.firstName}
                     autoComplete="firstName"
                     required
+                    disabled={doctor}
                     className="block w-full rounded-md border-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -80,6 +121,7 @@ export default function PatientEditPage() {
                     autoComplete="surname"
                     defaultValue={patient.surname}
                     required
+                    disabled={doctor}
                     className="block w-full rounded-md border-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -100,6 +142,7 @@ export default function PatientEditPage() {
                     defaultValue={patient.email}
                     autoComplete="email"
                     required
+                    disabled={doctor}
                     className="block w-full rounded-md border-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -107,26 +150,28 @@ export default function PatientEditPage() {
 
               <div>
                 <div className="flex items-center">
-                  <input defaultChecked={patient.active} id="active" type="checkbox" value="" className="mr-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"/>
+                  <input disabled={doctor} defaultChecked={patient.active} id="active" type="checkbox" value="" className="mr-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"/>
                   <label htmlFor="active" className="block text-sm font-medium leading-6 text-gray-900">
                       Active
                   </label>
                 </div>
               </div>
-
-              <div className="flex">
-                <button
-                  type="submit"
-                  className="flex w-6/12 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Save
-                </button>
-                <button
-                  className="flex ml-1 w-6/12 justify-center rounded-md bg-gray-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
-                >
-                  Delete
-                </button>
-              </div>
+                {!doctor &&
+                    <div className="flex">
+                        <button
+                        type="submit"
+                        className="flex w-6/12 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                        Save
+                        </button>
+                        <button
+                        onClick={handleDelete}
+                        className="flex ml-1 w-6/12 justify-center rounded-md bg-gray-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
+                        >
+                        Delete
+                        </button>
+                    </div>
+                }
             </form>
           </div>
         </div>
