f3c8af9c8183a61dfd84e317e436c09aa1b7be7f
Michal <mpkowalcz@wp.pl>
2024-01-26 01:07:25 +0100

    Cleanup of unused classes

--- MvcClinic/MvcClinic/DTOs/ErrorViewDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace MvcClinic.DTOs
-{
-    public class ErrorViewDTO
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
