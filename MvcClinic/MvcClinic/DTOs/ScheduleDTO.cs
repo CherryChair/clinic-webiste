ad705c9c64464b11ab6c90f0943371698a67a125
Michal <mpkowalcz@wp.pl>
2024-01-26 01:34:39 +0100

    Added scheduleDTO

---
+++ MvcClinic/MvcClinic/DTOs/ScheduleDTO.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcClinic.DTOs
+{
+    public class ScheduleDTO
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public PatientDTO? Patient { get; set; }
+        public EmployeeDTO? Doctor { get; set; }
+        public string? Description { get; set; }
+        public Guid ConcurrencyStamp { get; set; } = Guid.NewGuid();
+    }
+}
